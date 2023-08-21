802adb08420e6259c2860c315bcd379c45a14bd0
hopeforkoami <hopeforkoami@gmail.com>
2023-08-21 21:08:51 +0000

    update the design of the desktop homepage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+import './style.css';
+import logo from'./images/logo.png';
+import arrow_down from './images/arrow-down.png';
+import house_icon from './images/house-icon.svg';
+import pencil_square from './images/pencil-icon.svg';
+import shield_check from './images/shield-icon.svg';
+require('bootstrap-icons/font/bootstrap-icons.css');
+
 // Menu mobile
 const menuButton = document.querySelector('header button');
 const menuImg = menuButton.querySelector('img');
@@ -13,3 +21,14 @@ menuButton.addEventListener('click', () => {
   }
   menuButton.blur(); // Remove focus from button
 });
+//desktop menu
+const homeLogo = document.querySelector('.logoImg');
+homeLogo.src = logo;
+const dropdownMenu = document.querySelector('.menu_dropdown_arrow');
+dropdownMenu.src = arrow_down;
+const house_Icon = document.querySelector('.house_icon');
+house_Icon.src = house_icon;
+const pencil_Icon = document.querySelector('.pencil-square');
+const shiel_Check = document.querySelector('.shield-check');
+pencil_Icon.src = pencil_square;
+shiel_Check.src = shield_check;
--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,18 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        include: path.resolve(__dirname, './node_modules/bootstrap-icons/font/fonts'),
+        use: {
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'webfonts',
+                publicPath: '../webfonts',
+            },
+        }
+    },
     ],
   },
 };
