ac4cbbbcbf4abe8462d4b0fd4c6ab81726a95f80
hopeforkoami <hopeforkoami@gmail.com>
2023-09-01 00:04:02 +0000

    Estate display section

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import arrow_down from './images/arrow-down.png';
 import house_icon from './images/house-icon.svg';
 import pencil_square from './images/pencil-icon.svg';
 import shield_check from './images/shield-icon.svg';
-import house1 from './images/house1.jpg';
-import house2 from './images/house2.jpg';
-import house3 from './images/house3.jpg';
+
 require('bootstrap-icons/font/bootstrap-icons.css');
 
 // Menu mobile
@@ -35,9 +33,3 @@ const pencil_Icon = document.querySelector('.pencil-square');
 const shiel_Check = document.querySelector('.shield-check');
 pencil_Icon.src = pencil_square;
 shiel_Check.src = shield_check;
-const houseImg1 = document.querySelector('.estate_image1');
-const houseImg2 = document.querySelector('.estate_image2');
-const houseImg3 = document.querySelector('.estate_image3');
-houseImg1.src = house1;
-houseImg2.src = house2;
-houseImg3.src = house3;
